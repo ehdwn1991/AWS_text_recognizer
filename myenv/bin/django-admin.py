f9168eee0211c72c486809738d5df66c57b43fec
EdwardSon <ehdwn1991@gmail.com>
2018-07-30 12:32:45 +0900

    mac

---
+++ myenv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/Edwardson/Dropbox/Codex/Coding_in_Mac/aws_project/myenv/bin/python3.6
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
